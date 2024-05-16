f4f08fd720a65e359e05ff8484693c797d6ac6ab
geydsonrenan <extremyly10@gmail.com>
2024-05-15 22:15:39 -0300

    editing hotel data

--- selenium-extraction/beautifulSoup.py
+++ selenium-extraction/beautifulSoup.py
@@ -7,27 +7,15 @@ import sys
 class Beautifulsoup():
 # Parseie o HTML usando BeautifulSoup
     def __init__(self, html, checkin, checkout):
-        self.soup = BeautifulSoup(html, "html.parser")
+        self.run(html, checkin, checkout)
+
+    def run(self, html, checkin, checkout):
         self.checkin = checkin
         self.checkout = checkout
-        self.run()
-
-    def extract(self, soup):
-    # Encontre os elementos desejados e extraia informações
-        for hotel in soup.find_all("div", class_="c82435a4b8 a178069f51 a6ae3c2b40 a18aeea94d d794b7a0f7 f53e278e95 c6710787a4"):
-            try:
-                nome = hotel.find("div", class_="f6431b446c a15b38c233").text.replace(',', '')
-                preco = hotel.find("span", class_="f6431b446c fbfd7c1165 e84eb96b1f").text.replace('R$', '')
-                yield {
-                    "Hotel": nome,
-                    "Preço": float(preco),
-                    "Checkin": self.checkin,
-                    "Checkout": self.checkout,
-                }
-            except:
-                pass
-    def run(self):
-        resultado = list(self.extract(self.soup))
+        resultado = []
+        for i in html:
+            self.soup = BeautifulSoup(i, "html.parser")
+            resultado.append(list(self.extract(self.soup)))
 
         # Caminho do arquivo CSV
         output_csv_path = "resultado.csv"
@@ -35,7 +23,36 @@ class Beautifulsoup():
         # Abra o arquivo para escrita
         with open(output_csv_path, "w", encoding="utf-8") as file:
         # Escreva os dados
-            file.write("Hotel,Preço,Checkin,Checkout\n")
+            file.write("Hotel,Cidade,Preço,Checkin,Checkout,Dist_centro,Dist_praia\n")
 
-            for item in resultado:
-                file.write(f"{item['Hotel']},{item['Preço']},{item['Checkin']},{item['Checkout']}\n")
+            for itens in resultado:
+                for item in itens:
+                    file.write(f"{item['Hotel']},{item['Cidade']},{item['Preço']},{item['Checkin']},{item['Checkout']},{item['Dist_centro']},{item['Dist_praia']}\n")
+
+
+    def extract(self, soup):
+    # Encontre os elementos desejados e extraia informações
+        for hotel in soup.find_all("div", class_="c82435a4b8 a178069f51 a6ae3c2b40 a18aeea94d d794b7a0f7 f53e278e95 c6710787a4"):
+            try:
+                nome = hotel.find("div", class_="f6431b446c a15b38c233").text.replace(',', '')
+                preco = hotel.find("span", class_="f6431b446c fbfd7c1165 e84eb96b1f").text.replace('R$', '')
+                distance_centro = hotel.find("span", attrs={"data-testid": "distance"})
+                if distance_centro:
+                    distance_centro = distance_centro.text.replace(',', '.').replace(' km do centro', '')
+                distance_praia = hotel.find("span", class_="abf093bdfe b058f54b9a")
+                if distance_praia:
+                    distance_praia = distance_praia.text.replace(' m da praia', '').replace(',', '.')
+                cidade = hotel.find("h1", class_="af8fbdf136 d5f78961c3")
+                if cidade:
+                    cidade = cidade.text.split(":", 1)[0]
+                yield {
+                    "Hotel": nome,
+                    "Preço": float(preco),
+                    "Cidade": cidade,
+                    "Checkin": self.checkin,
+                    "Checkout": self.checkout,
+                    "Dist_centro": distance_centro,
+                    "Dist_praia": distance_praia
+                }
+            except NameError as e:
+                print(e)
--- selenium-extraction/spider.py
+++ selenium-extraction/spider.py
@@ -10,53 +10,61 @@ from beautifulSoup import Beautifulsoup
 # Inicialize o driver do Chrome
 driver = webdriver.Chrome()
 
+cidades = ["Sydney", "Recife", "Atenas", "Paris", "Nova York", "Tokyo", "Cidade do Cabo"]
+cidades_destid = ["-1603135", "-665520", "-814876", "-1456928", "20088325", "-246227", "-1217214"]
+
 checkin = '2024-05-21'
 checkout = '2024-05-22'
 
-# Abra a página que deseja raspar
-driver.get(f"https://www.booking.com/searchresults.pt-br.html?label=gx1-br-booking-booking-sd-nptd&sid=a77533c6921c5bf28c165cc6b7a6d38d&aid=348858&ss=Recife%2C+Pernambuco%2C+Brasil&ssne=Arcoverde&ssne_untouched=Arcoverde&lang=pt-br&src=searchresults&dest_id=-665520&dest_type=city&ac_position=0&ac_click_type=b&ac_langcode=xb&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=79a11d850e0f0110&ac_meta=GhA3OWExMWQ4NTBlMGYwMTEwIAAoATICeGI6BlJlY2lmZUAASgBQAA%3D%3D&checkin={checkin}&checkout={checkout}&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure")
+html = []
 
-# Espere alguns segundos para garantir que a página seja carregada completamente
-time.sleep(3)
+for i in range(len(cidades)):
+    # Abra a página que deseja raspar
+    time.sleep(5)
 
-try:
-    button = driver.find_element(By.XPATH, '//aria-label[@class="Ignorar informações de login."]')
-    button.click()
-except:
-    pass
-# Defina o número de vezes que deseja rolar para baixo
-num_scrolls = 100
+    driver.get(f"https://www.booking.com/searchresults.pt-br.html?label=gx1-br-booking-booking-sd-nptd&sid=a77533c6921c5bf28c165cc6b7a6d38d&aid=348858&ss=Recife%2C+Pernambuco%2C+Brasil&ssne=Arcoverde&ssne_untouched=Arcoverde&lang=pt-br&src=searchresults&dest_id={cidades_destid[i]}&dest_type=city&ac_position=0&ac_click_type=b&ac_langcode=xb&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=79a11d850e0f0110&ac_meta=GhA3OWExMWQ4NTBlMGYwMTEwIAAoATICeGI6BlJlY2lmZUAASgBQAA%3D%3D&checkin={checkin}&checkout={checkout}&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure")
 
-count = 0
+    # Espere alguns segundos para garantir que a página seja carregada completamente
+    time.sleep(8)
 
-# Para a primeira rolada, não precisamos clicar em um botão
-first_scroll = True
+    try:
+        button = driver.find_element(By.XPATH, '//button[@aria-label="Ignorar informações de login."]')
+        button.click()
+    except:
+        pass
+    # Defina o número de vezes que deseja rolar para baixo
+    num_scrolls = 100
 
-for _ in range(num_scrolls):
-    # Role a página para baixo
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    count += 1
-    # Aguarde um pouco para o conteúdo adicional ser carregado
-    time.sleep(3)
-    
-    # A partir da segunda rolada, clique no botão desejado
-    if not first_scroll:
-        # Localize o botão e clique nele
-        try:
-            button = driver.find_element(By.XPATH, '//button[@class="a83ed08757 c21c56c305 bf0537ecb5 f671049264 deab83296e af7297d90d"]')
-            button.click()
-        except:
-            break
-    
-    # Defina first_scroll como False após a primeira rolada
-    if count > 1:
-        first_scroll = False
+    count = 0
 
-# Capture o HTML após rolar para baixo várias vezes e clicar no botão
-html = driver.page_source
+    # Para a primeira rolada, não precisamos clicar em um botão
+    first_scroll = True
+
+    for _ in range(num_scrolls):
+        # Role a página para baixo
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        count += 1
+        # Aguarde um pouco para o conteúdo adicional ser carregado
+        time.sleep(3)
+        
+        # A partir da segunda rolada, clique no botão desejado
+        if not first_scroll:
+            # Localize o botão e clique nele
+            try:
+                button = driver.find_element(By.XPATH, '//button[@class="a83ed08757 c21c56c305 bf0537ecb5 f671049264 deab83296e af7297d90d"]')
+                button.click()
+            except:
+                break
+        
+        # Defina first_scroll como False após a primeira rolada
+        if count > 1:
+            first_scroll = False
+
+    # Capture o HTML após rolar para baixo várias vezes e clicar no botão
+    html.append(driver.page_source)
 
 
-# Feche o navegador
+    # Feche o navegador
 driver.quit()
 
 # # Salvar o HTML em um arquivo no mesmo diretório do script
